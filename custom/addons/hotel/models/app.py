6d43f50f1c300801e6af1c82689f7003d6e3d0db
grujic96 <aleksandar.grujicc@gmail.com>
2018-11-06 13:14:49 +0100

    test

--- custom/addons/hotel/models/app.py
+++ custom/addons/hotel/models/app.py
@@ -1,14 +1,5 @@
 import socket
 import threading
+from custom.addons.hotel.models.hotel import HotelRoom
 
 
-i = 0
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', 80))
-while i<=2:
-    print('proso')
-    data, address = sock.recvfrom(512)
-    print(data)
-    if data[3] == 241:
-        print(data)
-    i += 1
